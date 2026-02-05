693d44f7c47a68aeb1429751cea20cc3e5dd9a7f
Lucas André <lucas.andre@uvvnet.com.br>
2026-02-05 08:39:36 -0300

    add try/catch for dont crashed serveless vercel by:Lucas

--- src/configs/database.js
+++ src/configs/database.js
@@ -5,9 +5,7 @@ let pool;
 if(!pool){
     pool = new Pool({
         connectionString:process.env.DATABASE_URL,
-        ssl:{
-            rejectUnauthorized:false,
-        },
+        ssl:{rejectUnauthorized:false},
     });
 }
 
