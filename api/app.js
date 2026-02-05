65bcdf98fcc09af2db5a5c2be9ff9d4ccb42da55
Lucas André <lucas.andre@uvvnet.com.br>
2026-02-05 12:58:53 -0300

    cors in vercel.json

--- api/app.js
+++ api/app.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = require('../src/routes/tarefas.routes.js')
 const app = express();
-const cors = requre('cors');
 
-const port = process.env.PORT || 8080;
-const corsOptions = {
-  origin: 'http://127.0.0.1:5500', // Libera apenas o seu front específico
-  methods: 'GET,POST,PUT,DELETE',  // Métodos permitidos
-  optionsSuccessStatus: 200        // Para navegadores legados
-};
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/tarefas',router);
 
