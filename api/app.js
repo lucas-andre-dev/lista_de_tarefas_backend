9cb75fc855a724bedcb37ed448c5dac568b2816d
Lucas André <lucas.andre@uvvnet.com.br>
2026-02-05 09:30:19 -0300

    modified index.js for app.js

---
+++ api/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = require('../src/routes/tarefas.routes.js')
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use('/tarefas',router);
+
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`);
+})
+module.exports = app;
