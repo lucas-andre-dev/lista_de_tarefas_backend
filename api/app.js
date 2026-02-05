ce43c677be9d24d0b0ad850eb65c6709e461f516
Lucas André <lucas.andre@uvvnet.com.br>
2026-02-05 10:37:34 -0300

    cors

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = require('../src/routes/tarefas.routes.js')
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 8080;
-
+app.use(cors());
 app.use(express.json());
 app.use('/tarefas',router);
 
