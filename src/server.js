a56a054e20286109f8787883c7879be2b0eab8eb
Lucas André <lucas.andre@uvvnet.com.br>
2026-02-05 07:55:41 -0300

    config vercel.json

--- src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const porta = process.env.SERVER_PORT;
-
-app.listen(porta,()=>{
-    console.log('Servidor ativo na url: http://localhost:'+porta)
-})
