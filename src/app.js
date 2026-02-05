32179ff7658f76b8cc6e5aafb786f3594c3c38b9
Lucas André <lucas.andre@uvvnet.com.br>
2026-02-05 07:45:39 -0300

    enabling cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./routes/tarefas.routes.js')
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use('/tarefas',routes);
-
+app.use(cors({
+  origin: '*'
+}));
 module.exports = app;
